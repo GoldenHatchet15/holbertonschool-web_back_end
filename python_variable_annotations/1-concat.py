dc173e5fd50171830852894708c86395b6ba3fa8
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-25 00:55:27 -0400

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function 'concat'
+that concatenates two strings.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings str1 and str2,
+    and returns the resulting string.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenation of str1 and str2.
+    """
+    return str1 + str2
