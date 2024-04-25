c525701fdc1043ed455b6029d3ac81cc8366a9e3
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-25 00:59:04 -0400

    task 3

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import math
+"""
+This module contains a type-annotated function 'floor'
+that computes the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float n.
+
+    Parameters:
+    n (float): The float number from which the floor will be computed.
+
+    Returns:
+    int: The floor value of n.
+    """
+    return math.floor(n)
