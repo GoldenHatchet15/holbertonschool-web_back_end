cec128614d9442f3ea2319e4014aec5df11cba3d
GoldenHatchet15 <raphsant20@gmail.com>
2024-04-25 02:08:24 -0400

    tasks

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
+"""
+Defines an asynchronous coroutine wait_random that waits for a random delay.
+"""
+
 import asyncio
 import random
-"""
-This module contains a type-annotated coroutine 'wait_random'
-that takes in an integer argument and returns a float.
-"""
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that takes in an integer argument
+    Waits for a random delay between 0 and max_delay seconds.
+
+    Parameters:
+        max_delay (int): The maximum delay value (default 10).
+
+    Returns:
+        float: The random delay.
     """
-    delay = max_delay * random.random()
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
+
