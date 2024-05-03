4263fc370463431030b9f3c32e7f958ac1c41c98
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-03 03:01:00 -0400

    tasks

---
+++ NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" 8-all """
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection"""
+    documents = mongo_collection.find()
+    return list(documents)
