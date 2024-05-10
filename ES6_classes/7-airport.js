67f2751b82d496e9cef182cb36c32d941d9f9649
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 13:47:03 -0400

    tasks

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,24 +1,23 @@
 // 7-airport.js
 
 export default class Airport {
-    constructor(name, code) {
-      this._name = name;
-      this._code = code;
-    }
-  
-    // Getter for name
-    get name() {
-      return this._name;
-    }
-  
-    // Getter for code
-    get code() {
-      return this._code;
-    }
-  
-    // Customize the default string representation
-    toString() {
-      return `[object ${this.code}]`;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
   }
-  
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Customize the default string representation
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
