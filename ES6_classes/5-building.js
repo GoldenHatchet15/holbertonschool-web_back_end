356a9c2440a15e3dd8a806e7e85444d51fa1ea0d
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 13:40:21 -0400

    tasks

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,10 +2,7 @@
 
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Cannot instantiate from abstract class Building');
-    }
-    if (this.evacuationWarningMessage === undefined) {
+    if (new.target !== Building && !this.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
