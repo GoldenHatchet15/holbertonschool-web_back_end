2d81008acd6f9c5c603430210088ad23f59a4342
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 14:00:20 -0400

    tasks

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,5 +1,4 @@
-// 9-hoisting.js
-
+// Export the HolbertonClass
 export class HolbertonClass {
     constructor(year, location) {
       this._year = year;
@@ -15,9 +14,7 @@ export class HolbertonClass {
     }
   }
   
-  const class2019 = new HolbertonClass(2019, 'San Francisco');
-  const class2020 = new HolbertonClass(2020, 'San Francisco');
-  
+  // Export the StudentHolberton class
   export class StudentHolberton {
     constructor(firstName, lastName, holbertonClass) {
       this._firstName = firstName;
@@ -34,11 +31,17 @@ export class HolbertonClass {
     }
   }
   
+  // Instantiate HolbertonClass
+  const class2019 = new HolbertonClass(2019, 'San Francisco');
+  const class2020 = new HolbertonClass(2020, 'San Francisco');
+  
+  // Instantiate StudentHolberton
   const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
   const student2 = new StudentHolberton('John', 'Doe', class2020);
   const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
   const student4 = new StudentHolberton('Donald', 'Bush', class2019);
   const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
   
+  // Export the list of students
   export const listOfStudents = [student1, student2, student3, student4, student5];
   
