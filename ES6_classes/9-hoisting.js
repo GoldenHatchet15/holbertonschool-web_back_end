f69eb83ea1c2025c8ebb932537c1420036d6494a
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 13:55:56 -0400

    tasks

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,48 +1,44 @@
 // 9-hoisting.js
 
-// Define the HolbertonClass first
 export class HolbertonClass {
-  constructor(year, location) {
-    this._year = year;
-    this._location = location;
+    constructor(year, location) {
+      this._year = year;
+      this._location = location;
+    }
+  
+    get year() {
+      return this._year;
+    }
+  
+    get location() {
+      return this._location;
+    }
   }
-
-  get year() {
-    return this._year;
+  
+  const class2019 = new HolbertonClass(2019, 'San Francisco');
+  const class2020 = new HolbertonClass(2020, 'San Francisco');
+  
+  export class StudentHolberton {
+    constructor(firstName, lastName, holbertonClass) {
+      this._firstName = firstName;
+      this._lastName = lastName;
+      this._holbertonClass = holbertonClass;
+    }
+  
+    get fullName() {
+      return `${this._firstName} ${this._lastName}`;
+    }
+  
+    get fullStudentDescription() {
+      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    }
   }
-
-  get location() {
-    return this._location;
-  }
-}
-
-// Instances of HolbertonClass should be created after it's defined
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-// Define the StudentHolberton class
-export class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
-  }
-
-  get fullName() {
-    return `${this._firstName} ${this._lastName}`;
-  }
-
-  get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-  }
-}
-
-// Create instances of StudentHolberton after it's defined
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
-// Export the list of students
-export const listOfStudents = [student1, student2, student3, student4, student5];
+  
+  const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+  const student2 = new StudentHolberton('John', 'Doe', class2020);
+  const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+  const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+  const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+  
+  export const listOfStudents = [student1, student2, student3, student4, student5];
+  
