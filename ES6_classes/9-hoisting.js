504d6a735c7573cbe7f9d8808411a7fbb9d61997
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 14:07:23 -0400

    tasks

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-// Export the HolbertonClass
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -14,7 +13,9 @@ export class HolbertonClass {
   }
 }
 
-// Export the StudentHolberton class
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -26,21 +27,20 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName}`;
   }
 
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-// Instantiate HolbertonClass
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-// Instantiate StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export the list of students
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
