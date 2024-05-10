187c57353cd6245b31f0c27b3fd8c87592571188
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 04:37:51 -0400

    tasks

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,16 +1,14 @@
+export function getLast() {
+  return ' is okay';
+}
+
 export function taskFirst() {
-    const task = 'I prefer const when I can.';  // Use `const` because `task` is not reassigned
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';     // Use `let` because `combination` is reassigned
-    combination += getLast();
-  
-    return combination;
-  }
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
+}
