f873ad6ca1a7fab9befe4206c5ec64d8b9f01a22
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 13:11:50 -0400

    tasks

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,34 +1,33 @@
 // 3-currency.js
 
 export default class Currency {
-    constructor(code, name) {
-      this.code = code;
-      this.name = name;
-    }
-  
-    // Getter for code
-    get code() {
-      return this._code;
-    }
-  
-    // Setter for code
-    set code(newCode) {
-      this._code = newCode;
-    }
-  
-    // Getter for name
-    get name() {
-      return this._name;
-    }
-  
-    // Setter for name
-    set name(newName) {
-      this._name = newName;
-    }
-  
-    // Method to display full currency
-    displayFullCurrency() {
-      return `${this.name} (${this.code})`;
-    }
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
   }
-  
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Method to display full currency
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
