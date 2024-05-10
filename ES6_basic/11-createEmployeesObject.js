19a56c3556f648657f6789e8ff394a789412326c
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 06:38:17 -0400

    tasks

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
   return {
-    [departmentName]: employees, // Computed property name
+    [departmentName]: employees,
   };
 }
