228c42e7731040b9b5e62c02eb1f4388d16e9620
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 05:27:22 -0400

    tasks

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const initialTask = false;
+  const initialTask2 = true;
+  const resultTask = trueOrFalse ? true : initialTask;
+  const resultTask2 = trueOrFalse ? false : initialTask2;
 
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
-
-  return [task, task2];
+  return [resultTask, resultTask2];
 }
