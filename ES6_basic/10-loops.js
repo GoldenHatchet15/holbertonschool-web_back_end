827e42f30a0a47c571f1af2a2234f8f21adb2446
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-10 06:29:48 -0400

    tasks

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let newArray = []; // Use let to declare a new array to store modified values
-    for (const value of array) { // Using for...of for iterating through array values directly
-      newArray.push(appendString + value); // Append the string and push to the new array
-    }
-  
-    return newArray; // Return the new array to avoid mutating the original array
+  const newArray = []; // Use let to declare a new array to store modified values
+  for (const value of array) { // Using for...of for iterating through array values directly
+    newArray.push(appendString + value); // Append the string and push to the new array
   }
-  
+
+  return newArray; // Return the new array to avoid mutating the original array
+}
