1fb0b0df69450f994fb1ad0dde470efebfea48e3
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-17 00:21:59 -0400

    tasks

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,11 +1,5 @@
-// 0-promise.js
-
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulating an asynchronous operation
-        setTimeout(() => {
-            // You can add any condition here to resolve or reject the promise
-            resolve("Success");
-        }, 1000);
+    return new Promise((resolve) => {
+      resolve();
     });
-}
+  }
