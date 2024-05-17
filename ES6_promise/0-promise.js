ae14b337f5b4b4518c75c8d662ee6caa776cb243
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-17 19:27:07 -0400

    tasks

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
+export default function getResponseFromAPI () {
+  return new Promise((resolve) => {
     resolve();
-    });
-  }
-  
+  });
+}
