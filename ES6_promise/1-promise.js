ad39ee2ee466e29c5f20bd24318d7a7ad9598006
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-17 00:21:20 -0400

    tasks

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,12 +1,6 @@
-  
 // 1-promise.js
 
-/**
- * This function returns a promise that resolves or rejects based on the input boolean.
- * @param {boolean} success - Determines whether the promise should resolve or reject.
- * @returns {Promise<Object|Error>} A promise that resolves with an object if true, or rejects with an Error if false.
- */
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
         if (success) {
             resolve({
@@ -18,5 +12,3 @@ function getFullResponseFromAPI(success) {
         }
     });
 }
-
-export default getFullResponseFromAPI;
