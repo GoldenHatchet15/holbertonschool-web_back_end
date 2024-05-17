2d867d22fc83328c199608edec627dd5d19d52af
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-17 00:16:08 -0400

    tasks

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,10 +1,22 @@
-export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({ status: 200, body: 'Success' });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  }
   
+// 1-promise.js
+
+/**
+ * This function returns a promise that resolves or rejects based on the input boolean.
+ * @param {boolean} success - Determines whether the promise should resolve or reject.
+ * @returns {Promise<Object|Error>} A promise that resolves with an object if true, or rejects with an Error if false.
+ */
+function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({
+                status: 200,
+                body: 'Success'
+            });
+        } else {
+            reject(new Error('The fake API is not working currently'));
+        }
+    });
+}
+
+export default getFullResponseFromAPI;
