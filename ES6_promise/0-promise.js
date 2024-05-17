21dc08d438864dde02eecf89354033a44fa31abc
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-17 00:02:13 -0400

    tasks

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,17 +1,5 @@
-// 0-promise.js
-
-/**
- * Returns a promise that either resolves or rejects based on a condition.
- * @return {Promise<string>} A promise that might resolve with a success message or reject with an error.
- */
-export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      const success = true; // Simulate success or failure
-      if (success) {
-        resolve("Success: Data fetched successfully");
-      } else {
-        reject(new Error("Failure: Unable to fetch data"));
-      }
-    });
+function getResponseFromAPI() {
+    return Promise.resolve();
   }
   
+  export default getResponseFromAPI;
