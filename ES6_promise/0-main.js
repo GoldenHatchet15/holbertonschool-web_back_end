f6dc9d5764eaa82ed5e1ea8111c27e07d01c6d4d
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-17 00:18:03 -0400

    tasks

--- ES6_promise/0-main.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-// This is the main file that will be used to test 
-// the function getResponseFromAPI from 0-promise.js
-import getResponseFromAPI from "./0-promise.js";
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);  // This should log `true` to the console.
--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,14 +1,11 @@
 // 0-promise.js
 
-/**
- * This function returns a new Promise that resolves immediately.
- * @returns {Promise<void>} A promise that resolves with no value.
- */
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        // This promise resolves immediately. You can modify the behavior as needed.
-        resolve();
+        // Simulating an asynchronous operation
+        setTimeout(() => {
+            // You can add any condition here to resolve or reject the promise
+            resolve("Success");
+        }, 1000);
     });
 }
-
-export default getResponseFromAPI;
