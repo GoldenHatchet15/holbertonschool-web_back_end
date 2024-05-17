9c6576b9f40a21b7a8ce5c45f580c1878ce8ec9c
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-16 23:56:30 -0400

    tasks

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-// 0-promise.js
-
-/**
- * This function returns a promise that resolves immediately.
- * @return {Promise<void>} A promise that resolves without returning any value.
- */
 export default function getResponseFromAPI () {
   return new Promise((resolve) => {
     resolve();
