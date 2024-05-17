f9e7204d6a9ea0d3edee00a99e02577797f401ec
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-17 19:54:45 -0400

    tasks

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,15 +1,13 @@
 // 2-then.js
-
-export default function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => {
-            console.log('Got a response from the API');
-            return {
-                status: 200,
-                body: 'success',
-            };
-        })
-        .catch(() => {
-            return new Error();
-        });
+function handleResponseFromAPI(promise) {
+    promise.then((result) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    }).catch((error) => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
+  
+export default handleResponseFromAPI;
+  
