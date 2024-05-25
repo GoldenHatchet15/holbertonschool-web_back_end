89f9c8ab4fce07e0b33029278229489c98561258
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-25 19:45:03 -0400

    tasks

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,11 @@
-const getListStudentIds = (students) => {
+function getListStudentIds(students) {
     if (!Array.isArray(students)) {
       return [];
     }
-    return students.map((student) => student.id);
-  };
+    return students.map(function (student) {
+      return student.id;
+    });
+  }
   
   export default getListStudentIds;
   
