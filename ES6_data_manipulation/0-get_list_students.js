e067ee037385e8332fdf8796938f85a47ef50ffe
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-25 19:29:17 -0400

    tasks

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,11 @@
-export default function getListStudents() {
-  return [
-    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
-    { firstName: 'James', id: 2, location: 'Columbia' },
-    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+function getListStudents() {
+  const obj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return obj;
 }
+
+export default getListStudents;
