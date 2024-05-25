ee481eab0cdb731a587e7c85596daaec759b8129
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-25 19:07:24 -0400

    tasks

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,14 @@
-const getListStudents = () => [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-];
+// 0-get_list_students.js
+
+const getListStudents = (function() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+  return function() {
+    return students;
+  };
+})();
 
 export default getListStudents;
