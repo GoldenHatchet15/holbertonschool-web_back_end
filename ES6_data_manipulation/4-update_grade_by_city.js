175fe2b9baaa84f648305ab9ecd96910fe861e18
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-25 22:25:22 -0400

    tasks

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,15 @@
-const findGrade = (newGrades, studentId) => {
-  const gradeObj = newGrades.find((grade) => grade.studentId === studentId);
-  return gradeObj ? gradeObj.grade : 'N/A';
-};
+const updateStudentGradeByCity = (students, city, newGrades) => {
+  const gradesMap = newGrades.reduce((acc, { studentId, grade }) => {
+    acc[studentId] = grade;
+    return acc;
+  }, {});
 
-const updateStudentGradeByCity = (students, city, newGrades) => students
-  .filter((student) => student.location === city)
-  .map((student) => ({
-    ...student,
-    grade: findGrade(newGrades, student.id),
-  }));
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => ({
+      ...student,
+      grade: gradesMap[student.id] || 'N/A',
+    }));
+};
 
 export default updateStudentGradeByCity;
