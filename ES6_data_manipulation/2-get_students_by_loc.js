c76fef754130f65f7c0ba16eed737d8b698331d3
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-25 20:02:26 -0400

    tasks

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,8 @@
-const getStudentsByLocation = (students, city) => students.filter((student) => student.location === city);
+const getStudentsByLocation = (students, city) => {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+    return students.filter((student) => student.location === city);
+}
 
 export default getStudentsByLocation;
