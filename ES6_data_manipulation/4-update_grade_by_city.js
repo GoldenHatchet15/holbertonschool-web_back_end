a538a1f3df60664d5e5912727a9df0f562b40a7e
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-27 21:54:29 -0400

    tasks

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,8 @@
-const updateStudentGradeByCity = (students, city, newGrades) => {
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-      return { ...student, grade: gradeObj ? gradeObj.grade : 'N/A' };
-    });
-};
+const updateStudentGradeByCity = (students, city, newGrades) => students
+  .filter((student) => student.location === city)
+  .map((student) => {
+    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+    return { ...student, grade: gradeObj ? gradeObj.grade : 'N/A' };
+  });
 
 export default updateStudentGradeByCity;
