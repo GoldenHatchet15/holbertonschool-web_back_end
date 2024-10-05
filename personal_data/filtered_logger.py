8f92ce40466b0f75745a39c5d531c16873bbebd7
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-05 11:44:56 -0400

    Log Formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """
 Filtering module
 """
+import logging
 import re
 from typing import List
 
@@ -24,3 +25,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(rf"{re.escape(field)}=(.*?){re.escape(separator)}",
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class for filtering PII data """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialize the formatter with a list of fields to redact """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log message by redacting sensitive information """
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
