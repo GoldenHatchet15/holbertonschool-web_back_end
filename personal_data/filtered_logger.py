c5046fbd00b4ce6a369148f77b2d35bff88e65aa
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-05 12:32:30 -0400

    Connect to secure db

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -82,22 +82,23 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> connection.MySQLConnection:
     """
-    Connects to the MySQL database using 
-    credentials from environment variables.
-
-    Returns:
-        connection.MySQLConnection: A connector to the MySQL database.
+    Connects to the MySQL database using credentials from environment variables.
     """
-    # Get environment variables for DB credentials
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     database = os.getenv('PERSONAL_DATA_DB_NAME')
 
-    # Create and return the MySQL connection
+    # Print the environment variables to verify
+    print(f"Username: {username}")
+    print(f"Password: {password}")
+    print(f"Host: {host}")
+    print(f"Database: {database}")
+
     return mysql.connector.connect(
         user=username,
         password=password,
         host=host,
         database=database
     )
+
