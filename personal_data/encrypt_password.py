9b7c1746420fe88f9de8a56e62326eeb58b3303d
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-08 06:23:48 -0400

    chk valid psswd

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Encrypting passwords module using bcrypt
+Encrypting and validating passwords module using bcrypt
 """
 import bcrypt
 
@@ -22,3 +22,19 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The plain-text password to validate.
+
+    Returns:
+        bool: True if the password matches, False otherwise.
+    """
+    # Use bcrypt's checkpw to verify
+    # if the password matches the hashed password
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
