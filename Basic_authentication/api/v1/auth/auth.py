911ad501ad5386be0601b25e1a72fc7c5a3a8620
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-09 17:41:43 -0400

    define routes

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,9 +10,24 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method that checks if a path requires authentication
+        """ Method to check if a path requires authentication
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Ensure that both path and excluded_paths are slash tolerant
+        if path[-1] != '/':
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
+
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get the Authorization header from the request
