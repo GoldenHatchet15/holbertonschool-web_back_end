2a6691108fafccecb8673167d63c82b4478bac6a
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-12 22:52:03 -0400

    User object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ BasicAuth class for basic authentication
 """
+from typing import Tuple, TypeVar, Optional
 import base64
+from models.user import User
 from api.v1.auth.auth import Auth
-from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -52,3 +53,23 @@ class BasicAuth(Auth):
         # Split the string into email and password
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> Optional[User]:
+        """ Returns the User instance based on the email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        # Search for the user by email
+        user = User.search({"email": user_email})
+        if not user:
+            return None
+
+        # User is found; check the password
+        if user[0].is_valid_password(user_pwd):
+            return user[0]
+
+        return None
