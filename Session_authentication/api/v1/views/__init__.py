c73acff5707a44d676aa7bcd9165624f6c2ec61c
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-13 21:23:34 -0400

    New view for Session Authentication

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" Initialize views for the API """
+""" DocDocDocDocDocDoc
+"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Import all views
 from api.v1.views.index import *
 from api.v1.views.users import *
-from api.v1.views.session_auth import *  
-# Import the session_auth view
+from api.v1.views.session_auth import *
+
+User.load_from_file()
