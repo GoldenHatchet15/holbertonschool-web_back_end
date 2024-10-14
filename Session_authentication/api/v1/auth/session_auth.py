8b5d150ebf5d1dbe911656b4fd3a3dce56ede0ff
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-13 20:56:23 -0400

    Usr ID for Session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -22,3 +22,12 @@ class SessionAuth(Auth):
 
         # Return the session ID
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Retrieve the user ID based on the session ID """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        # Return the user ID for the given session ID using the dictionary's
+        # get method
+        return self.user_id_by_session_id.get(session_id, None)
