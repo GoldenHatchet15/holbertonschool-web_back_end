414359034582f8b514c8a39503d27477bca97e0a
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-17 15:28:36 -0400

    Get reset psswd token

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -8,6 +8,7 @@ from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
 
+
 @app.route('/users', methods=['POST'])
 def register_user():
     """POST /users route to register a new user"""
@@ -22,6 +23,7 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 @app.route('/sessions', methods=['POST'])
 def login():
@@ -43,6 +45,7 @@ def login():
     response.set_cookie("session_id", session_id)
     return response
 
+
 @app.route('/sessions', methods=['DELETE'])
 def logout():
     """DELETE /sessions route to log out a user"""
@@ -58,6 +61,7 @@ def logout():
     AUTH.destroy_session(user.id)
     return redirect('/')
 
+
 @app.route('/profile', methods=['GET'])
 def profile():
     """GET /profile route to retrieve a user's profile"""
@@ -71,6 +75,7 @@ def profile():
         abort(403)
 
     return jsonify({"email": user.email}), 200
+
 
 @app.route('/reset_password', methods=['POST'])
 def get_reset_password_token():
@@ -87,6 +92,7 @@ def get_reset_password_token():
     except ValueError:
         # If the email is not found, return 403
         abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
