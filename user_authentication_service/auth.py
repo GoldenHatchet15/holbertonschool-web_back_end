2eaa7df3cf096dd9a03a448526c858777db57538
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-17 12:53:26 -0400

    hash password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password with bcrypt
+
+    Args:
+        password (str): The password to be hashed
+
+    Returns:
+        bytes: The hashed password as bytes
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
