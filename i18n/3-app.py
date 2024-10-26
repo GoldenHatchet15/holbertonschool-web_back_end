90eededc501bddf002824d8f55d2c81910cd8ccc
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-26 16:15:50 -0400

    fix

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""Start a basic Flask app."""
+"""
+Flask app with i18n support using Flask-Babel.
+"""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
@@ -7,33 +10,45 @@ app = Flask(__name__)
 
 
 class Config:
-    """Configuration class for Babel and Flask app."""
+    """
+    Configuration class for Babel and Flask app.
+
+    Attributes:
+        LANGUAGES (list): Supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default language locale.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-babel = Babel()
+babel = Babel(app)
 
 
 def get_locale():
     """
-    Determine the best match
-    for supported languages based
-    on request headers.
+    Determine the best match for supported languages based on request headers.
+
+    Returns:
+        str: Best matching language from the supported languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# Initialize Babel with locale selector function
 babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
 def index():
-    """Route to render the index page."""
-    return render_template('2-index.html')
+    """
+    Render the index page with translated text.
+
+    Returns:
+        str: Rendered HTML of the index page.
+    """
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
