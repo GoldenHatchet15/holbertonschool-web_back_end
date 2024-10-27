c299ee8c50dc033c269854494034088889c3d949
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-27 04:21:54 -0400

    Force locale with URL parameter

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,40 +1,62 @@
 #!/usr/bin/env python3
-""" Basic Flask app module.
 """
+Flask app with i18n support using Flask-Babel.
+"""
+
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
-    """ Language and time zone settings.
+
+class Config:
     """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    Configuration class for Babel and Flask app.
+
+    Attributes:
+        LANGUAGES (list): Supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default language locale.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
 
 
+def get_locale():
+    """
+    Determine the best match for supported languages based on request args
+    and headers.
+
+    Returns:
+        str: The locale to be used for translations.
+    """
+    # Check if the 'locale' parameter is in the URL and is supported
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    # Fallback to the best match from request headers
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route('/')
-def hello():
-    """ Hello method.
+def index():
+    """
+    Render the index page with translated text.
+
+    Returns:
+        str: Rendered HTML of the index page.
     """
     return render_template('4-index.html')
 
 
-@babel.localeselector
-def get_locale():
-    """ Method to determine the best match with our supported languages.
-    """
-    local = request.args.get('locale')
-    if local and local in app.config['LANGUAGES']:
-        return local
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(debug=True)
