aa2cd1473304617032de7644f4db69b4b57a3a88
GoldenHatchet15 <raphsant20@gmail.com>
2024-11-10 15:17:39 -0400

    OLDSCHOOL BAND

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,6 @@
--- List all bands with Glam as their main style and rank by their longevity.
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan  DESC;
+-- Script to list Glam rock bands ranked by their longevity
+SELECT band_name,
+       IFNULL(YEAR(split) - YEAR(formed), YEAR(CURDATE()) - YEAR(formed)) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
