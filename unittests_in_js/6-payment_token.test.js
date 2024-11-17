47dedf879d954080d4eb1b13e280233308c6517c
GoldenHatchet15 <raphsant20@gmail.com>
2024-11-17 01:18:19 -0400

    Async test with done

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -8,6 +8,6 @@ describe('getPaymentTokenFromAPI', () => {
       expect(response).to.be.an('object');
       expect(response).to.have.property('data', 'Successful response from the API');
       done(); // Notify Mocha that the test is complete
-    }).catch((er) => done(err)); // Ensure done is called on error
+    }).catch((err) => done(err)); // Ensure done is called on error
   });
 });
