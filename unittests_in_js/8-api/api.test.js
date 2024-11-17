43ba72130cae8316da0b40b2fdcfa13fa92e3413
GoldenHatchet15 <raphsant20@gmail.com>
2024-11-17 01:45:23 -0400

    Basic integration testing

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,39 +1,25 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
 describe('Index page', () => {
-  let server;
-  let port;
-
-  before((done) => {
-    server = app.listen(0, () => { // Use port 0 for a random available port
-      port = server.address().port; // Get the assigned port
-      console.log(`Server started on port ${port}`);
-      done();
-    });
-  });
-
-  after(() => {
-    if (server) server.close();
-  });
+  const BASE_URL = 'http://localhost:7865';
 
   it('should return the correct status code', (done) => {
-    request.get(`http://localhost:${port}/`, (err, res, body) => {
+    request.get(`${BASE_URL}/`, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       done();
     });
   });
 
   it('should return the correct message', (done) => {
-    request.get(`http://localhost:${port}/`, (err, res, body) => {
+    request.get(`${BASE_URL}/`, (err, res, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
 
   it('should handle unknown routes with a 404 status code', (done) => {
-    request.get(`http://localhost:${port}/unknown`, (err, res, body) => {
+    request.get(`${BASE_URL}/unknown`, (err, res, body) => {
       expect(res.statusCode).to.equal(404);
       done();
     });
